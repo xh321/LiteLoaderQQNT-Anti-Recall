6127068013b96864b0504cdd869c413700cfb235
xh321 <xiaohe321@outlook.com>
2023-07-09 17:18:11 +0800

    解决新版QQ撤回无提示的问题

--- renderer.js
+++ renderer.js
@@ -1,8 +1,18 @@
 export function onLoad() {
     anti_recall.recallTip((event, msgId) => {
         console.log("[Anti-Recall]", "尝试反撤回消息ID", msgId);
-        document.querySelector(
-            `div[id='${msgId}-msgContent'] > span > span`
-        ).innerText += "\n[该消息已被撤回]";
+
+        try {
+            document.querySelector(
+                `div[id='${msgId}-content'] > span > span`
+            ).innerText += "\n[该消息已被撤回]";
+        } catch {}
+        
+        try {
+            //兼容旧版本
+            document.querySelector(
+                `div[id='${msgId}-msgContent'] > span > span`
+            ).innerText += "\n[该消息已被撤回]";
+        } catch {}
     });
 }
